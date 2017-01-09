0d8baeeb304e1e6dece97bb8d3efe154c52258f8
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2017-01-09 14:45:50 +0000

    Added some sql table creation thing

--- bad-code/web/index.php
+++ bad-code/web/index.php
@@ -1,8 +1,39 @@
 <?php
 include 'includes/header.php';
+include 'frontend/homepage.html';
+$a[] = "Anna";
+$a[] = "Brittany";
+$a[] = "Cinderella";
+$a[] = "Diana";
+$a[] = "Eva";
+$a[] = "Fiona";
+$a[] = "Gunda";
+$a[] = "Hege";
+$a[] = "Inga";
+$a[] = "Johanna";
+$a[] = "Kitty";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Ophelia";
+$a[] = "Petunia";
+$a[] = "Amanda";
+$a[] = "Raquel";
+$a[] = "Cindy";
+$a[] = "Doris";
+$a[] = "Eve";
+$a[] = "Evita";
+$a[] = "Sunniva";
+$a[] = "Tove";
+$a[] = "Unni";
+$a[] = "Violet";
+$a[] = "Liza";
+$a[] = "Elizabeth";
+$a[] = "Ellen";
+$a[] = "Wenche";
+$a[] = "Vicky";
+echo $_SERVER['REQUEST_URI'];
 ?>
 
-<h1> Hello World!!! </h1>
 
 <!-- 1337 c0d3 -->
 
