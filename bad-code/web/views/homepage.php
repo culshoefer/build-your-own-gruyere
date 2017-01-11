b438e6f2f3cd437d695275427b64a2ed7ec03649
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2017-01-11 09:54:34 +0000

    Merge master

--- bad-code/web/views/homepage.php
+++ bad-code/web/views/homepage.php
@@ -8,31 +8,31 @@ $GLOBALS['page_name'] = 'Home';
 include 'includes/header.php';
 ?>
 
-<div class="container" id="allSnippets">
-    <h2>List of Users</h2>
-</div>
+    <div class="container" id="allSnippets">
+        <h2>List of Users</h2>
+    </div>
 
-<script type="text/javascript">
-    $(document).ready(function() {
-        $.get('/api/overview', function(data) {
-            function addChild(username, user_id, last_snippet) {
-                $.get('/api/settings?user_id=' + user_id, function(data) {
-                    $('#allSnippets').append('<div class="card"> \
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $.get('/api/overview', function (data) {
+                function addChild(username, user_id, last_snippet) {
+                    $.get('/api/settings?user_id=' + user_id, function (data) {
+                        $('#allSnippets').append('<div class="card"> \
                                             <span class="card-title" style=color:' + data.profilecolour + '>' + username + '</span>\
                                             <div class="card-content">\
                                                 <p>' + last_snippet + '</p>\
                                                 <a href="snippets?user_id=' + username + '">All snippets</a>\
                                             </div>\
                                           </div>');
-                });
-            }
+                    });
+                }
 
-            data.forEach(function(entry) {
-                addChild(entry.username, entry.user_id, entry.last_snippet);
+                data.forEach(function (entry) {
+                    addChild(entry.username, entry.user_id, entry.last_snippet);
+                });
             });
         });
-    });
-</script>
+    </script>
 
 <?php
 include 'includes/footer.php';
